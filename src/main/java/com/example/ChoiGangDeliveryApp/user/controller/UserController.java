877763d45c06f004766a6790b6d6a02e1b02f5df
diung <diunimwork@gmail.com>
2024-10-11 10:28:51 +0900

    usercontroller

--- src/main/java/com/example/ChoiGangDeliveryApp/user/controller/UserController.java
+++ src/main/java/com/example/ChoiGangDeliveryApp/user/controller/UserController.java
@@ -3,11 +3,15 @@ package com.example.ChoiGangDeliveryApp.user.controller;
 import com.example.ChoiGangDeliveryApp.jwt.JwtTokenUtils;
 import com.example.ChoiGangDeliveryApp.jwt.dto.JwtRequestDto;
 import com.example.ChoiGangDeliveryApp.jwt.dto.JwtResponseDto;
+import com.example.ChoiGangDeliveryApp.user.dto.PasswordChangeRequestDto;
+import com.example.ChoiGangDeliveryApp.user.dto.PasswordDto;
 import com.example.ChoiGangDeliveryApp.user.dto.UserCreateDto;
 import com.example.ChoiGangDeliveryApp.user.dto.UserDto;
 import com.example.ChoiGangDeliveryApp.user.service.UserService;
 import io.jsonwebtoken.Jwt;
+import org.springframework.web.multipart.MultipartFile;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -36,6 +40,104 @@ public class UserController {
     ){
         return service.login(dto);
     }
+
+    @PostMapping("sendVerifyCode")
+    public void sendVerifyCode (
+            @RequestBody()
+            String receiverEmail
+    ){
+        service.sendVerifyCode(receiverEmail);
+    }
+
+    @PostMapping("verifyEmail")
+    public void verifyEmail (
+            @RequestBody()
+            String email,
+            String code
+    ){
+        service.verifyEmail(email, code);
+    }
+
+    @PostMapping("generateRandomNumber")
+    public String generateRandomNumber (
+            @RequestBody()
+            int len
+    ){
+        return service.generateRandomNumber(len);
+    }
+
+    @PostMapping("signUpSendCode")
+    public void signUpSendCode (
+            @RequestBody()
+            String email
+    ){
+        service.signUpSendCode(email);
+    }
+
+
+    @PostMapping("passwordSendCode")
+    public ResponseEntity<String> passwordSendCode (
+            @RequestBody()
+            String email
+    ){
+         return service.passwordSendCode(email);
+    }
+
+
+    @PostMapping("resetPassword")
+    public ResponseEntity<String> resetPassword (
+            @RequestBody()
+            PasswordChangeRequestDto requestDto
+    ){
+         return service.resetPassword(requestDto);
+    }
+
+    @PostMapping("changePassword")
+    public ResponseEntity<String> changePassword (
+            @RequestBody()
+            PasswordDto dto
+    ){
+         return service.changePassword(dto);
+    }
+
+    @PostMapping("/uploadProfileImage")
+    public void uploadProfileImage (
+            @RequestParam("image")
+            MultipartFile image
+    )throws Exception {
+        service.uploadProfileImage(image);
+    }
+
+    @GetMapping("/getMyProfile")
+    public UserDto getMyProfile() {
+        return service.getMyProfile();
+    }
+
+    @PostMapping("/requestOwnerRole")
+    public void requestOwnerRole(
+            @RequestParam("businessNumber")
+            String businessNumber
+    ){
+        service.requestOwnerRole(businessNumber);
+    }
+
+    @PostMapping("/requestDriverRole")
+    public void requestDriverRole(
+            @RequestParam("licenseNumber")
+            String licenseNumber
+    ){
+        service.requestDriverRole(licenseNumber);
+    }
+
+
+
+
+
+
+
+
+
+
 
     @GetMapping("/validate")
     public String validateTest(
